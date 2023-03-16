1425297116c3fbef645d6862553df30baad0b1cd
Saveleva Natalia <n.saveleva@itssupport.ru>
2023-03-16 16:16:14 +0400

    Change system property

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,11 +17,12 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserPosition = "0x0";
         Configuration.browser = System.getProperty("browser", "chrome");
-        System.getProperty("version", "100.0");
-        Configuration.remote = "https://user1:1234@" + System.getProperty("baseUrl", "selenoid.autotests.cloud/") + "wd/hub";
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        System.getProperty("browser_version", "100.0");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
